ec8d3417be80b3b61d2d84bcdf35ec2f15436e3d
Copulatrix <maher.chris@outlook.com>
2020-02-16 23:44:06 +1100

    init

--- Scripts/Updated.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-file_put_contents('/app/Public/.htaccess', file_get_contents('https://raw.githubusercontent.com/fadebit/fadebit/master/Data/Htaccess'));
-file_put_contents('/app/Public/.htpasswd', file_get_contents('https://raw.githubusercontent.com/fadebit/fadebit/master/Data/Htpasswd'));
